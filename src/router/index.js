783061d3b06f7ab326ece498710c763416a7fbd1
Heba Skhail <heba236ah@gmail.com>
2025-04-11 14:46:52 +0200

    Add Login Page UI & API

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginPage from '../components/Login/LoginPage.vue';
+import TaskPage from '../components/Task/TaskPage.vue'
+
+
+const routes = [
+    {
+        path: '/',
+        name: 'Login',
+        component: LoginPage,
+    },
+    {
+        path: '/tasks',
+        name: 'Tasks',
+        component: TaskPage,
+    }
+];
+const router = createRouter({
+    history: createWebHistory(import.meta.env.VITE_API_URL),
+    routes,
+});
+
+export default router;
